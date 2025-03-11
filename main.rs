2e866fc545fadfd460f09306546a23938355ed0b
Moinuddin Masud <moinuddinmasud@gmail.com>
2025-03-11 01:06:10 -0700

    more variable shadowing

--- main.rs
+++ main.rs
@@ -10,6 +10,10 @@ fn main() {
     let x = x / 2;
     println!("The answer is {}", x);
 
+    let x = 5 + 3;
+    let x = x * (6 - 2);
+    println!("{}", x);
+
     let name: &str = "James";
     let age: i8 = 34;
     println!("The answer is {} and age {}", name, age);
