086f2a5f7945c18be4d918e25da638c5d6dd1b08
Moinuddin Masud <moinuddinmasud@gmail.com>
2025-03-11 01:28:59 -0700

    some interesting block rust

--- main.rs
+++ main.rs
@@ -39,4 +39,23 @@ fn main(/* you can touch this */) {
 
     // it will print
     let _print = println!("This will print even though variable is unused.");
+
+    // Interesting block
+    let x: i32 = { 4 + 5 };
+    println!("4 + 5 == {}", x);
+
+    let x: i32 = {
+        6 + 7; // this will give us a warning but nor error
+        println!("I will be print!");
+        4 + 5 //; putting semicolon in the end of the block will give us error
+        // because rust return () for this block
+    };
+    println!("4 + 5 == {}", x);
+
+    let z: i32 = { // you can think the block is anonymous and run immediate
+        let x = 3 * 5;
+        println!("The value of z is {}", x);
+        x
+    };
+    println!("z is {}", z);
 }
